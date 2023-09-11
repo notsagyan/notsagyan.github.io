ca48d23539964a3f269b47e1010c016d78603e0d
Sagyan Singh <sagyansingh@Sagyans-MacBook-Air.local>
2023-09-11 10:10:01 +0545

    Updated slider.

--- src/components/slider/js/slider.js
+++ src/components/slider/js/slider.js
@@ -1,7 +1,10 @@
-import { useEffect, cloneElement } from "react";
+import { useEffect, useRef, useState, cloneElement } from "react";
 import '../css/slider.css';
 
 const Slider = (props) => {
+    const [autoScroll, setAutoScroll] = useState(true);
+    const slider = useRef();
+
     const transformChildren = () => {
         var element = props.children.map((item, index) => {
             return cloneElement(item, {
@@ -13,11 +16,25 @@ const Slider = (props) => {
         return element;
     }
 
-    useEffect(() => {
+    const toggleAutoScroll = (bool) => {
+        setAutoScroll(bool);
+    }
 
-    }, []);
+    useEffect(() => {
+        console.log(autoScroll);
+        const id = setInterval(() => {
+            if (autoScroll){
+                slider.current.scrollBy({
+                    left: 100,
+                    behavior: 'smooth'
+                }); 
+            }
+        }, 1000);
+
+        return () => clearInterval(id);
+    }, [autoScroll]);
     return (
-        <div className="slider-container">
+        <div className="slider-container" ref={slider} onMouseOver={() => toggleAutoScroll(false)} onMouseLeave={() => toggleAutoScroll(true)}>
             <i class="fa-solid fa-chevron-left slider-left"></i>
             {transformChildren()}
             <i class="fa-solid fa-chevron-right slider-right"></i>
